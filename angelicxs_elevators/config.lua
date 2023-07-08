e86f2ccc0447d8f8fdbc4d2859d3a762902a00fa
AngelicXS <99211701+GouveiaXS@users.noreply.github.com>
2023-07-08 07:51:51 -0600

    Update config.lua
    Added third eye toggle

--- angelicxs_elevators/config.lua
+++ angelicxs_elevators/config.lua
@@ -5,6 +5,7 @@ Config = {}
 Config.UseESX = true						-- Use ESX Framework
 Config.UseQBCore = false					-- Use QBCore Framework (Ignored if Config.UseESX = true)
 
+Config.UseThirdEye = true				-- If true uses third eye.
 Config.ThirdEyeName = 'qb-target' 			-- Name of third eye aplication
 Config.Use3DText = true                        -- Use 3D text to interact
 Config.NHMenu = false						-- Use NH-Context [https://github.com/whooith/nh-context]
